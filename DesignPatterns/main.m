7bf6a076196a20a21146ed339f7b39d1a17ca5d0
Rui Pereira <rui.pedro.barroso@gmail.com>
2014-02-01 20:38:04 +0000

    Initial commit

---
+++ DesignPatterns/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DesignPatterns
+//
+//  Created by cuca on 01/02/14.
+//  Copyright (c) 2014 Rui Pereira. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "spAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([spAppDelegate class]));
+    }
+}
---
+++ DesignPatterns/spAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  spAppDelegate.h
+//  DesignPatterns
+//
+//  Created by cuca on 01/02/14.
+//  Copyright (c) 2014 Rui Pereira. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface spAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
